29f4c21e45418b0ca995b113a20ff7a8678dc467
Ralf Herzog <ralf@rherzog.de>
2017-12-13 21:59:49 +0100

    docker-compose poller added. init.sh 'is_poller?' check

--- init.sh
+++ init.sh
@@ -256,6 +256,16 @@ echo "DB connection is ok"
 QUERY="SELECT count(*) FROM information_schema.tables WHERE table_schema = '${DB_NAME}';"
 COUNT=$(mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} ${DB_PASS:+-p$DB_PASS} -ss -e "${QUERY}")
 
+# Poller container?
+IS_POLLER=${IS_POLLER:-0}
+if [ "${IS_POLLER}" == "1" ]
+then
+  # disable updates
+  sed -i "/\$config\['update'\].*;/d" /data/config/config.php
+  echo "\$config['update'] = 0;" >> /data/config/config.php
+fi
+
+
 # setup update channel
 UPDATE_CHANNEL=${UPDATE_CHANNEL:-master}
 sed -i "/\$config\['update_channel'\].*;/d" /data/config/config.php
